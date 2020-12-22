81f683c24f22c33f6061fce8576fa9dd35f102e5
zhuwenjoyce <Disney201912>
2020-12-22 20:39:03 +0800

    init project

---
+++ src/test/java/com/joyce/boot/docker/JoyceBootDockerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.joyce.boot.docker;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JoyceBootDockerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
